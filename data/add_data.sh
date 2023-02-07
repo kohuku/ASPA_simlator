0c663feaa5a7047be96b0463ef6ef419cda4a2bd
kohuku <60776229+kohuku@users.noreply.github.com>
2023-02-07 12:26:11 +0900

    Update add_data.sh

--- data/add_data.sh
+++ data/add_data.sh
@@ -5,18 +5,18 @@ shift
 
 while [ $# -gt 0 ]
 do
-  # for i in `seq 0 9`
-  # do
-  #   wget "https://api.bgpview.io/asn/$1"
-  #   if [ $? -eq 0  ]
-  #   then
-  #     break
-  #   fi
-  # done
-  # echo -n "$1 " >> "data/detail/detail_dic$num"
-  # cat $1 >> "data/detail/detail_dic$num"
-  # echo "" >> "data/detail/detail_dic$num"
-  # rm $1
+  for i in `seq 0 9`
+  do
+    wget "https://api.bgpview.io/asn/$1"
+    if [ $? -eq 0  ]
+    then
+      break
+    fi
+  done
+  echo -n "$1 " >> "data/detail/detail_dic$num"
+  cat $1 >> "data/detail/detail_dic$num"
+  echo "" >> "data/detail/detail_dic$num"
+  rm $1
 
   for i in `seq 0 9`
   do
@@ -31,18 +31,18 @@ do
   echo "" >> "data/peer/peer_dic$num"
   rm peers
 
-  # for i in `seq 0 9`
-  # do
-  #   wget "https://api.bgpview.io/asn/$1/downstreams"
-  #   if [ $? -eq 0 ]
-  #   then
-  #     break
-  #   fi
-  # done
-  # echo -n "$1 " >> "data/down/down_dic$num"
-  # cat downstreams >> "data/down/down_dic$num"
-  # echo "" >> "data/down/down_dic$num"
-  # rm downstreams
+  for i in `seq 0 9`
+  do
+    wget "https://api.bgpview.io/asn/$1/downstreams"
+    if [ $? -eq 0 ]
+    then
+      break
+    fi
+  done
+  echo -n "$1 " >> "data/down/down_dic$num"
+  cat downstreams >> "data/down/down_dic$num"
+  echo "" >> "data/down/down_dic$num"
+  rm downstreams
 
   shift
 done
